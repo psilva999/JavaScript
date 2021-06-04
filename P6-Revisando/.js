d64c100ddd1a0ba47fc2c1cb29d5323d27ca2d35
Wanderson Pedrosa <p.silva0399@gmail.com>
2021-06-04 13:00:58 -0300

    Revisão de JS

---
+++ P6-Revisando/.js
@@ -0,0 +1,11 @@
+  //variável armazenando uma pergunta ao usuário
+  var nome = prompt ("Qual o seu nome")
+
+  //Presença de ${} envolvido com crases para facilitar o processo
+  alert (`Prazer te conhecer, ${nome}`)
+
+  var number1 = 10
+  var number2 = 20
+  var total = number1 + number2
+
+  alert (`A soma entre ${number1} e ${number2} é igual á: ${total}`)
