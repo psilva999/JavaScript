654e0a26ac65eb817e77c543f8c71421782719ad
Wanderson Pedrosa <p.silva0399@gmail.com>
2021-06-07 12:17:10 -0300

    Modifiquei mais dois arquivos .js

--- P5-Revisando/js/aula3.js
+++ P5-Revisando/js/aula3.js
@@ -1,1 +1,14 @@
+  var aprendendo = 'JavaScript teste'
 
+  /* length conta quantas letras tem na variável ou frase
+  (espaço, hífen e vírgula entre as palavras ou frases também conta) */
+  alert (`A palavra ${aprendendo} tem ${aprendendo.length} letras `)
+
+  // Deixa letra minúscula
+  alert (aprendendo.toLowerCase())
+
+  // Deixa maiúscula
+  alert (aprendendo.toUpperCase())
+
+  // document.write mostra a variável no documento HTML
+  document.write(aprendendo)
--- P5-Revisando/js/aula4.js
+++ P5-Revisando/js/aula4.js
@@ -1,1 +1,15 @@
+  var n = 250.779898
+  var dolar = 1000
 
+  // Exemplo de toFixed ajustando a quantidade de casas depois do ponto
+  alert (`Sem toFixed: ${n}`)
+  alert (`Com toFixed: ${n.toFixed(2)}`)
+
+  // replace trocando o ponto pela vírgula
+  alert (`Com toFixed e replace: ${n.toFixed(2).replace('.', ',')}`)
+
+  /* toLocaleString localizará a string indicada (pt-BR nesse caso)
+     currency - indica que uma valor monetário será utilizado */
+  alert (`${n.toLocaleString('pt-BR', {style:'currency', currency: 'BRL'}) }`)
+
+  alert (`${n.toLocaleString('pt-BR', {style:'currency', currency: 'USD'}).replace(',', '.') }`)
