551da2283e9b1e9f1ac3f7e18f627cf6638b6684
Wanderson Pedrosa <p.silva0399@gmail.com>
2021-07-01 14:16:35 -0300

    Aprendi a utilizar o 'while' e 'do'

---
+++ P16-Repeat/1-while-do/script.js
@@ -0,0 +1,15 @@
+var n1 = 6
+
+// Estrutura de repetição com teste lógico no inicio
+while (n1>0) {
+    console.log (`Passo: ${n1}`)
+    n1--
+}
+
+//Estrutura de repetição com teste lógico no final
+do {
+    console.log (`Passo: ${n1}`)
+    n1 = n1 - 2
+} while (n1>0)
+
+//SÓ FUNCIONA se desabilitar algum deles
